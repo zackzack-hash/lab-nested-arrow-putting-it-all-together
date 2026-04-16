a6a9257e9ad33d68e90151a862f066093a401f19
zakaria <azaki6777@gmail.com>
2026-04-16 21:56:28 +0300

    new commit

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
+//create function createLoginTracker
+function createLoginTracker(userInfo){
 
+  const {userName, password} = userInfo;
+  
+  //initialize attemptCount
+  let attemptCount = 0;
 
+  //create and return an arrow function
+  return (passwordAttempt) => {
+    //Increment attemptcount by 1 everytime the user tries to login 
+    attemptCount++;
+    
+    if (passwordAttempt === password && attemptCount < 3){
+      return "Login successful"
+    } else if (passwordAttempt !== password && attemptCount <= 3){
+        return `Attempt ${attemptCount}: Incorrect password. Please try again.`
+    } else{
+        return "Account locked due to too many failed login attempts"
+    }
+  }
 
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
+}
